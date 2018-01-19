b35d705810e7a5c57991dbb38c70a72e8126159f
Jake VanderPlas <jakevdp@gmail.com>
2018-01-18 19:28:47 -0800

    BUG: decode bytes before reading JSON

--- pdvega/schema.py
+++ pdvega/schema.py
@@ -1,4 +1,5 @@
 from pkgutil import get_data
 from json import loads
 
-VEGALITE_SCHEMA = loads(get_data('pdvega', 'vega-lite-v2.0.4.json'))
+VEGALITE_FILE = 'vega-lite-v2.0.4.json'
+VEGALITE_SCHEMA = loads(get_data('pdvega', VEGALITE_FILE).decode('utf-8'))
