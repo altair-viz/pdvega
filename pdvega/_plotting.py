627bdb651ddb56b9d5f2faaae4dcdd4b20a116d9
Jake VanderPlas <jakevdp@gmail.com>
2018-01-23 20:25:28 -0800

    ENH: add kde/density plots

--- pdvega/_plotting.py
+++ pdvega/_plotting.py
@@ -1,3 +1,6 @@
+import numpy as np
+import pandas as pd
+
 from ._utils import infer_vegalite_type
 from ._pandas_internals import FramePlotMethods, SeriesPlotMethods
 from vega3 import Vega, VegaLite
@@ -12,7 +15,8 @@ INTERACTIVE_SCALES = {
 }
 
 
-def vgplot_df_line(df, x=None, y=None, interactive=True, width=450, height=300):
+def vgplot_df_line(df, x=None, y=None, interactive=True, width=450, height=300,
+                   var_name='variable', value_name='value'):
     if x is None:
         if df.index.name is None:
             df.index.name = 'index'
@@ -24,15 +28,15 @@ def vgplot_df_line(df, x=None, y=None, interactive=True, width=450, height=300):
         assert y in df.columns
         df = df[[x, y]]
 
-    df = df.melt([x], var_name='variable', value_name='value')
+    df = df.melt([x], var_name=var_name, value_name=value_name)
 
     D = {
       "$schema": "https://vega.github.io/schema/vega-lite/v2.json",
       "mark": "line",
       "encoding": {
         "x": {"field": x, "type": infer_vegalite_type(df[x])},
-        "y": {"field": "value", "type": infer_vegalite_type(df["value"])},
-        "color": {"field": "variable", "type": infer_vegalite_type(df["variable"], ordinal_threshold=10)}
+        "y": {"field": value_name, "type": infer_vegalite_type(df[value_name])},
+        "color": {"field": var_name, "type": infer_vegalite_type(df[var_name], ordinal_threshold=10)}
       },
       "width": width,
       "height": height
@@ -383,6 +387,38 @@ def vgplot_df_hexbin(df, x, y, C=None, reduce_C_function=None, gridsize=100,
     return VegaLite(spec, data=df)
 
 
+def vgplot_df_kde(df, y=None, bw_method=None, interactive=True,
+                  width=450, height=300):
+    from scipy.stats import gaussian_kde
+
+    if y is not None:
+        df = df[y].to_frame()
+
+    tmin, tmax = df.min().min(), df.max().max()
+    trange = tmax - tmin
+    t = np.linspace(tmin - 0.5 * trange, tmax + 0.5 * trange, 1000)
+
+    kde_df = pd.DataFrame({col: gaussian_kde(df[col], bw_method=bw_method).evaluate(t)
+                           for col in df}, index=t)
+    kde_df.index.name = ' '
+
+    return vgplot_df_line(kde_df, value_name='Density')
+
+
+def vgplot_series_kde(ser, bw_method=None, interactive=True,
+                      width=450, height=300):
+    from scipy.stats import gaussian_kde
+
+    tmin, tmax = ser.min(), ser.max()
+    trange = tmax - tmin
+    t = np.linspace(tmin - 0.5 * trange, tmax + 0.5 * trange, 1000)
+
+    kde_ser = pd.Series(gaussian_kde(ser, bw_method=bw_method).evaluate(t),
+                        index=t, name=ser.name)
+    kde_ser.index.name = ' '
+    return vgplot_series_line(kde_ser)
+
+
 class FrameVgPlotMethods(FramePlotMethods):
     def __call__(self, x=None, y=None,
                  kind='line', interactive=True,
@@ -408,6 +444,9 @@ class FrameVgPlotMethods(FramePlotMethods):
         elif kind == 'hexbin':
             return vgplot_df_hexbin(self._data, x=x, y=y, interactive=interactive,
                                     width=width, height=height, **kwds)
+        elif kind in ['kde', 'density']:
+            return vgplot_df_kde(self._data, y=y, interactive=interactive,
+                                 width=width, height=height, **kwds)
         else:
             raise NotImplementedError("kind = {0}".format(kind))
 
@@ -430,5 +469,8 @@ class SeriesVgPlotMethods(SeriesPlotMethods):
         elif kind == 'hist':
             return vgplot_series_hist(self._data, interactive=interactive,
                                       width=width, height=height, **kwds)
+        elif kind in ['kde', 'density']:
+            return vgplot_series_kde(self._data, interactive=interactive,
+                                     width=width, height=height, **kwds)
         else:
             raise NotImplementedError("kind = {0}".format(kind))
