7e71f9830044b1c24da3469af92a5cd740e6f25c
Zsailer <zachsailer@gmail.com>
2018-01-24 20:37:55 -0800

    pandas-like register functions for backwards comp

--- pdvega/__init__.py
+++ pdvega/__init__.py
@@ -1,5 +1,7 @@
-from ._monkeypatch import monkeypatch_pandas
+from ._register import register
 from ._core import scatter_matrix
 
+# Register new methods with pandas objects.
+register()
 
 __version__ = '0.1.dev0'
--- pdvega/_pandas_internals.py
+++ pdvega/_pandas_internals.py
@@ -1,14 +1,34 @@
 from pandas.plotting._core import FramePlotMethods, SeriesPlotMethods
 
 try:
-    from pandas.api.extensions import (register_dataframe_accessor,
-                                       register_series_accessor)
-except ImportError:
-    from pandas.core.accessor import AccessorProperty
-except ImportError:  # Pandas before 0.22.0
-    from pandas.core.base import AccessorProperty
-
-try:
     from pandas.api.types import infer_dtype as infer_dtype
 except ImportError:  # Pandas before 0.20.0
     from pandas.lib import infer_dtype as infer_dtype
+
+from pandas import DataFrame, Series
+
+try:
+    from pandas.api.extensions import (register_dataframe_accessor,
+                                       register_series_accessor)
+except ImportError:
+    try:
+        from pandas.core.accessor import AccessorProperty
+    except ImportError:  # Pandas before 0.22.0
+        from pandas.core.base import AccessorProperty
+
+    # Define register functions
+    class register_dataframe_accessor(object):
+        """Register custom accessor on DataFrame."""
+        def __init__(self, name):
+            self.name = name
+
+        def __call__(self, accessor):
+            setattr(DataFrame, self.name, AccessorProperty(accessor, accessor))
+
+    class register_series_accessor(object):
+        """Register custom accessor on Series."""
+        def __init__(self, name):
+            self.name = name
+
+        def __call__(self, accessor):
+            setattr(Series, self.name, AccessorProperty(accessor, accessor))
