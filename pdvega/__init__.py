dc423e20b4027983d6d1e5fa6f60a14c95416aeb
Jake VanderPlas <jakevdp@gmail.com>
2018-01-31 20:58:49 -0800

    MAINT: bump version to 0.2.0.dev0

--- pdvega/__init__.py
+++ pdvega/__init__.py
@@ -3,4 +3,4 @@ from ._axes import Axes
 from ._core import FramePlotMethods, SeriesPlotMethods
 from .plotting import scatter_matrix, andrews_curves, parallel_coordinates, lag_plot
 
-__version__ = '0.1'
+__version__ = '0.2.0.dev0'
